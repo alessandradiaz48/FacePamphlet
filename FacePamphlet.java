8874e365d187bb548466fd4dd444d59b5d3368c3
alessandradiaz48 <alessandradiaz48@gmail.com>
2017-03-10 15:03:52 -0500

    Update FacePamphlet.java

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -1,93 +1,133 @@
 /*
- * File: FacePamphletDatabase.java
- * -------------------------------
- * This class keeps track of the profiles of all users in the
- * FacePamphlet application.  Note that profile names are case
- * sensitive, so that "ALICE" and "alice" are NOT the same name.
+ * File: FacePamphletProfile.java
+ * ------------------------------
+ * This class keeps track of all the information for one profile
+ * in the FacePamphlet social network.  Each profile contains a
+ * name, an image (which may not always be set), a status (what 
+ * the person is currently doing, which may not always be set),
+ * and a list of friends.
  */
 
+import acm.graphics.*;
 import java.util.*;
 
-public class FacePamphletDatabase implements FacePamphletConstants {
+public class FacePamphletProfile implements FacePamphletConstants {
 
-	/**
-	 * The HashMap that stores the names of the profiles and the
-	 * FacePamphletProfileS associated with the name
-	 */
-	private HashMap<String, FacePamphletProfile> profiles;
+	/** The name of a profile */
+	private String profileName;
+
+	/** The image assigned to a profile */
+	private GImage profileImage;
+
+	/** The status assigned to a profile */
+	private String profileStatus;
+
+	/** The ArrayList that stores the friends of a certain profile */
+	private ArrayList<String> friends = new ArrayList<String>();
 
 	/**
 	 * Constructor This method takes care of any initialization needed for the
-	 * database.
+	 * profile.
 	 */
-	public FacePamphletDatabase() {
-		profiles = new HashMap<String, FacePamphletProfile>();
+	public FacePamphletProfile(String name) {
+		// Initially starts with an almost empty profile
+		profileName = name;
+		profileImage = null;
+		profileStatus = "";
+		friends = new ArrayList<String>();
+	}
+
+	/** This method returns the name associated with the profile. */
+	public String getName() {
+		return profileName;
 	}
 
 	/**
-	 * This method adds the given profile to the database. If the name
-	 * associated with the profile is the same as an existing name in the
-	 * database, the existing profile is replaced by the new profile passed in.
+	 * This method returns the image associated with the profile. If there is no
+	 * image associated with the profile, the method returns null.
 	 */
-	public void addProfile(FacePamphletProfile profile) {
-		// Checks if the profile you want to add had already been added
-		if (!profiles.containsValue(profile)) {
-			profiles.put(profile.getName(), profile);
-			// If the profile has already been added, it will update the profile
-			// in the database
-		} else {
-			profiles.put(profile.getName(), profile);
-		}
+	public GImage getImage() {
+		return profileImage;
+	}
+
+	/** This method sets the image associated with the profile. */
+	public void setImage(GImage image) {
+		profileImage = image;
 	}
 
 	/**
-	 * This method returns the profile associated with the given name in the
-	 * database. If there is no profile in the database with the given name, the
-	 * method returns null.
+	 * This method returns the status associated with the profile. If there is
+	 * no status associated with the profile, the method returns the empty
+	 * string ("").
 	 */
-	public FacePamphletProfile getProfile(String name) {
-		if (profiles.containsKey(name)) {
-			return profiles.get(name);
-		} else {
-			return null;
-		}
+	public String getStatus() {
+		return profileStatus;
+	}
+
+	/** This method sets the status associated with the profile. */
+	public void setStatus(String status) {
+		profileStatus = status;
 	}
 
 	/**
-	 * This method removes the profile associated with the given name from the
-	 * database. It also updates the list of friends of all other profiles in
-	 * the database to make sure that this name is removed from the list of
-	 * friends of any other profile.
-	 * 
-	 * If there is no profile in the database with the given name, then the
-	 * database is unchanged after calling this method.
+	 * This method adds the named friend to this profile's list of friends. It
+	 * returns true if the friend's name was not already in the list of friends
+	 * for this profile (and the name is added to the list). The method returns
+	 * false if the given friend name was already in the list of friends for
+	 * this profile (in which case, the given friend name is not added to the
+	 * list of friends a second time.)
 	 */
-	public void deleteProfile(String name) {
-		if (profiles.containsKey(name)) {
-			// The iterator shows the friends for a given profile
-			Iterator<String> iterator = getProfile(name).getFriends();
-			// Makes sure that there are friends to check through
-			while (iterator.hasNext()) {
-				String friend = iterator.next();
-				// Removes the name of the profile you want to delete from the
-				// list of friends of the profiles who previously were had the
-				// profile deleted as a friend.
-				getProfile(friend).removeFriend(name);
-			}
-			// Removes the profile itself
-			profiles.remove(name);
-		}
-	}
-
-	/**
-	 * This method returns true if there is a profile in the database that has
-	 * the given name. It returns false otherwise.
-	 */
-	public boolean containsProfile(String name) {
-		if (profiles.containsKey(name)) {
+	public boolean addFriend(String friend) {
+		if (!friends.contains(friend)) {
+			friends.add(friend);
 			return true;
 		} else {
 			return false;
 		}
+	}
+
+	/**
+	 * This method removes the named friend from this profile's list of friends.
+	 * It returns true if the friend's name was in the list of friends for this
+	 * profile (and the name was removed from the list). The method returns
+	 * false if the given friend name was not in the list of friends for this
+	 * profile (in which case, the given friend name could not be removed.)
+	 */
+	public boolean removeFriend(String friend) {
+		if (friends.contains(friend)) {
+			friends.remove(friend);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * This method returns an iterator over the list of friends associated with
+	 * the profile.
+	 */
+	public Iterator<String> getFriends() {
+		Iterator<String> iterator = friends.iterator();
+		return iterator;
+	}
+
+	/**
+	 * This method returns a string representation of the profile. This string
+	 * is of the form: "name (status): list of friends", where name and status
+	 * are set accordingly and the list of friends is a comma separated list of
+	 * the names of all of the friends in this profile.
+	 * 
+	 * For example, in a profile with name "Alice" whose status is "coding" and
+	 * who has friends Don, Chelsea, and Bob, this method would return the
+	 * string: "Alice (coding): Don, Chelsea, Bob"
+	 */
+	public String toString() {
+		String profileInfo = new String("");
+		profileInfo += getName() + " (" + getStatus() + "): ";
+		if (friends != null) {
+			for (int i = 0; i < friends.size(); i++) {
+				profileInfo += friends.get(i) + ", ";
+			}
+		}
+		return profileInfo;
 	}
 }
